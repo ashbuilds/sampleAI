5e93141a58dc648e63973e6ccb5410e3bd934000
theasia-Ashish <ashish.mishra@theasia.com>
2017-09-25 22:04:43 +0700

    [fix] webpack-less compile in prod

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -157,7 +157,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.(?:css|less)$/,
             loader: ExtractTextPlugin.extract(
                 Object.assign(
                     {
@@ -179,26 +179,6 @@ module.exports = {
                             minimize: true,
                             sourceMap: shouldUseSourceMap,
                             localIdentName: '[name]__[local]__[hash:base64:7]',
-                          },
-                        },
-                        {
-                          loader: require.resolve('postcss-loader'),
-                          options: {
-                            // Necessary for external CSS imports to work
-                            // https://github.com/facebookincubator/create-react-app/issues/2677
-                            ident: 'postcss',
-                            plugins: () => [
-                              require('postcss-flexbugs-fixes'),
-                              autoprefixer({
-                                browsers: [
-                                  '>1%',
-                                  'last 4 versions',
-                                  'Firefox ESR',
-                                  'not ie < 9', // React doesn't support IE8 anyway
-                                ],
-                                flexbox: 'no-2009',
-                              }),
-                            ],
                           },
                         },
                       ],
